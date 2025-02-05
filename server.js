b01ca7f3edc95ece1651b595b171334d9bd14f67
Muhamad Yopan Ramadhan <yopanramadhan8@gmail.com>
2025-02-05 12:33:53 +0700

    fix

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 
 require('dotenv').config();
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, './')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, './' , 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
